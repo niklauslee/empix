2b2660208a5a55ae66d5f21140fcba58991ebb1a
niklauslee <niklaus.lee@gmail.com>
2026-07-10 09:47:55 +0900

    setup tailwindcss

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,7 +1,7 @@
 export function Test() {
   return (
     <div>
-      <h1>Test Component</h1>
+      <h1 className="text-2xl font-bold text-green-100">Test Component</h1>
       <p>This is a test component.</p>
     </div>
   );
