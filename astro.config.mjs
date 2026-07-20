d0ef58bd369b82b4f70559babd364294c9d8ef57
niklauslee <niklaus.lee@gmail.com>
2026-07-20 14:37:16 +0900

    add cloudflare plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  adapter: cloudflare(),
 });
